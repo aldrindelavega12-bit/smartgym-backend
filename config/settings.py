cab5aa2a12184add03162032c54d9ad6e202a60a
aldrindelavega12-bit <aldrindelavega12@gmail.com>
2026-05-16 00:51:59 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ DB_CONFIG = {
     "password": os.getenv("bRQXvdMKWBmfeaABLzmdOVaCqkmzonoL"),
     "database": os.getenv("railway"),
     "port": int(os.getenv("50506"))
-
+}
 # System Settings
 FP_COOLDOWN_SECONDS = 3
 FACE_THRESHOLD = 60
