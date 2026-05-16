b264bad892fad7d633b0ce76637768dda060e09e
Aldrin <aldrindelavega12@gmail.com>
2026-05-17 00:45:44 +0800

    update

--- config/settings.py
+++ config/settings.py
@@ -8,13 +8,13 @@ DB_CONFIG = {
     "port": 50506 
 }
 
-#DB_CONFIG = {
- #   "host": "127.0.0.1",
-  #  "user": "smartgym",
-   # "password": "smartgym123",
-    #"database": "smart_gym_db",
-    #"port": 3306
-#}
+# DB_CONFIG = {
+#     "host": "127.0.0.1",
+#     "user": "smartgym",
+#     "password": "smartgym123",
+#     "database": "smart_gym_db",
+#     "port": 3306
+# }
 
 # System Settings
 FP_COOLDOWN_SECONDS = 3
