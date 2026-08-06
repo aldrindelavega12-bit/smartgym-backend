9948a572c8fe31e1af78bf87548d21deb71e528a
Aldrin <aldrindelavega12@gmail.com>
2026-08-06 21:02:15 +0800

    update

--- api/api_server.py
+++ api/api_server.py
@@ -27,8 +27,6 @@ socketio = SocketIO(
 API_KEY = "GYM_MASTER_2026"
 RENDER_API = "https://smartgym-api-ia2e.onrender.com"
 
-
-@app.route("/api/activate_account", methods=["POST"])
 def activate_account():
 
     data = request.get_json()
@@ -38,7 +36,6 @@ def activate_account():
     password = data.get("password")
 
     if not token or not username or not password:
-
         return jsonify({
             "success": False,
             "message": "Missing required fields."
@@ -51,51 +48,64 @@ def activate_account():
 
         # CHECK TOKEN
         cursor.execute("""
-            SELECT member_id
-            FROM member_activation
-            WHERE activation_token=%s
-            AND status='PENDING'
+            SELECT
+                a.member_id,
+                m.full_name
+            FROM member_activation a
+            JOIN members m
+                ON a.member_id = m.id
+            WHERE a.activation_token=%s
+            AND a.status='PENDING'
             LIMIT 1
         """, (token,))
 
         activation = cursor.fetchone()
 
         if not activation:
-
             return jsonify({
                 "success": False,
                 "message": "Invalid activation link."
             })
 
         member_id = activation["member_id"]
+        fullname = activation["full_name"]
 
         # CHECK DUPLICATE USERNAME
         cursor.execute("""
             SELECT id
             FROM user_accounts
             WHERE username=%s
-            LIMIT 1
         """, (username,))
 
         if cursor.fetchone():
-
             return jsonify({
                 "success": False,
                 "message": "Username already exists."
             })
 
-        # UPDATE ACCOUNT
+        # INSERT ACCOUNT
         cursor.execute("""
-            UPDATE user_accounts
-            SET
-                username=%s,
-                password=%s,
-                role='member'
-            WHERE user_id=%s
+            INSERT INTO user_accounts
+            (
+                user_id,
+                fullname,
+                username,
+                password,
+                role
+            )
+            VALUES
+            (
+                %s,
+                %s,
+                %s,
+                %s,
+                'member'
+            )
         """, (
+            member_id,
+            fullname,
             username,
-            password,
-            member_id
+            password
         ))
 
         # MARK TOKEN USED
